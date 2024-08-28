7472effb41808b24fe076bffc2df6cf12922061a
Glenn Torrens <glenntorrens@gmail.com>
2024-08-28 19:09:00 +0900

    first react app to folder

---
+++ react-news-app/src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./Components/NavBar";
+import NewsBoard from "./Components/NewsBoard";
+import { useState } from "react";
+
+
+const App = () => {
+ 
+  const [category, setCategory]= useState("general")
+  return (
+    <div>
+      <NavBar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+      
+    </div>
+  );
+};
+
+export default App;
